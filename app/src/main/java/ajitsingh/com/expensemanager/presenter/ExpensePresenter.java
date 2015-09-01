363615703c76d15fcba04b3db47f7db7bc946404
AjitSingh <jeetsingh.ajit@gmail.com>
2015-09-02 00:33:19 +0530

    store current date instead of datetime in db as it will help to consolidate expenses in db itself

--- app/src/main/java/ajitsingh/com/expensemanager/presenter/ExpensePresenter.java
+++ app/src/main/java/ajitsingh/com/expensemanager/presenter/ExpensePresenter.java
@@ -2,14 +2,13 @@ package ajitsingh.com.expensemanager.presenter;
 
 import android.widget.ArrayAdapter;
 
-import java.util.Date;
 import java.util.List;
 
 import ajitsingh.com.expensemanager.database.ExpenseDatabaseHelper;
 import ajitsingh.com.expensemanager.model.Expense;
 import ajitsingh.com.expensemanager.view.ExpenseView;
 
-import static ajitsingh.com.expensemanager.utils.DateUtil.getCurrentDateTime;
+import static ajitsingh.com.expensemanager.utils.DateUtil.getCurrentDate;
 
 public class ExpensePresenter {
 
@@ -22,7 +21,7 @@ public class ExpensePresenter {
   }
 
   public void addExpense() {
-    Expense expense = new Expense(Long.valueOf(view.getAmount()), view.getType(), getCurrentDateTime());
+    Expense expense = new Expense(Long.valueOf(view.getAmount()), view.getType(), getCurrentDate());
     database.addExpense(expense);
   }
 
