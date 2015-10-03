cfa003b391e2bf2ff912f108ad392a84795262f6
AjitSingh <jeetsingh.ajit@gmail.com>
2015-10-03 10:11:28 +0530

    adding more tests for database methods

--- app/src/androidTest/java/ajitsingh/com/expensemanager/database/ExpenseDatabaseHelperTest.java
+++ app/src/androidTest/java/ajitsingh/com/expensemanager/database/ExpenseDatabaseHelperTest.java
@@ -10,6 +10,7 @@ import org.junit.Before;
 import org.junit.Test;
 import org.junit.runner.RunWith;
 
+import java.util.ArrayList;
 import java.util.List;
 
 import ajitsingh.com.expensemanager.model.Expense;
@@ -19,6 +20,7 @@ import ajitsingh.com.expensemanager.table.ExpenseTypeTable;
 
 import static android.support.test.InstrumentationRegistry.getTargetContext;
 import static org.hamcrest.core.Is.is;
+import static org.hamcrest.core.IsCollectionContaining.hasItems;
 import static org.junit.Assert.assertThat;
 import static org.junit.Assert.assertTrue;
 
@@ -76,6 +78,45 @@ public class ExpenseDatabaseHelperTest {
     List<Expense> expenses = database.getExpensesForCurrentMonthGroupByCategory();
 
     assertThat(expenses.size(), is(2));
+  }
+
+  @Test
+  public void shouldReturnTodaysExpenses() throws Exception {
+    database.addExpense(new Expense(200l, "Food", "02-10-2015"));
+
+    List<Expense> expenses = database.getTodaysExpenses();
+
+    assertThat(expenses.size(), is(1));
+    assertThat(expenses.get(0).getAmount(), is(200l));
+    assertThat(expenses.get(0).getType(), is("Food"));
+    assertThat(expenses.get(0).getDate(), is("02-10-2015"));
+  }
+
+  @Test
+  public void shouldReturnTodaysExpensesForMultipleExpenseTypes() throws Exception {
+    database.addExpense(new Expense(200l, "Food", "02-10-2015"));
+    database.addExpense(new Expense(300l, "Travel", "02-10-2015"));
+
+    List<Expense> expenses = database.getTodaysExpenses();
+
+    assertThat(expenses.size(), is(2));
+  }
+
+  @Test
+  public void shouldReturnCurrentWeeksExpenses() throws Exception {
+    database.addExpense(new Expense(200l, "Food", "02-10-2015"));
+    database.addExpense(new Expense(100l, "Food", "02-10-2015"));
+    database.addExpense(new Expense(300l, "Travel", "03-10-2015"));
+    database.addExpense(new Expense(400l, "Movie", "04-10-2015"));
+
+    List<Expense> expenses = database.getCurrentWeeksExpenses();
+    ArrayList<String> expenseTypes = new ArrayList<>();
+    for (Expense expense : expenses) {
+      expenseTypes.add(expense.getType());
+    }
+
+    assertThat(expenses.size(), is(3));
+    assertThat(expenseTypes, hasItems("Food", "Travel", "Movie"));
   }
 
   private void freezeDate(String date) {
