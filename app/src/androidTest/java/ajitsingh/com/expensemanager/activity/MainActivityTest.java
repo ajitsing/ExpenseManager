3eea20de356df56341f4ddc32cecc3b8b5ca34f3
AjitSingh <jeetsingh.ajit@gmail.com>
2015-11-05 23:00:01 +0530

    adding first espresso test

---
+++ app/src/androidTest/java/ajitsingh/com/expensemanager/activity/MainActivityTest.java
@@ -0,0 +1,29 @@
+package ajitsingh.com.expensemanager.activity;
+
+import android.content.Intent;
+import android.support.test.rule.ActivityTestRule;
+import android.support.test.runner.AndroidJUnit4;
+
+import org.junit.Rule;
+import org.junit.Test;
+import org.junit.runner.RunWith;
+
+import ajitsingh.com.expensemanager.R;
+
+import static android.support.test.espresso.Espresso.onView;
+import static android.support.test.espresso.assertion.ViewAssertions.matches;
+import static android.support.test.espresso.matcher.ViewMatchers.withId;
+import static android.support.test.espresso.matcher.ViewMatchers.withText;
+
+
+@RunWith(AndroidJUnit4.class)
+public class MainActivityTest {
+  @Rule
+  public ActivityTestRule activityTestRule = new ActivityTestRule(MainActivity.class);
+
+  @Test
+  public void shouldDisplayExpenseButton() throws Exception {
+    activityTestRule.launchActivity(new Intent());
+    onView(withId(R.id.add_expense)).check(matches(withText("Add Expense")));
+  }
+}
