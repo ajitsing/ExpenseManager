5112c4e9ce90eeb9997faf867cf662e7a74d86bf
AjitSingh <jeetsingh.ajit@gmail.com>
2016-04-30 10:32:08 +0530

    keeping a simple test for now

--- app/src/androidTest/java/ajitsingh/com/expensemanager/activity/MainActivityTest.java
+++ app/src/androidTest/java/ajitsingh/com/expensemanager/activity/MainActivityTest.java
@@ -1,7 +1,5 @@
 package ajitsingh.com.expensemanager.activity;
 
-import android.content.Intent;
-import android.support.test.espresso.contrib.DrawerActions;
 import android.support.test.rule.ActivityTestRule;
 import android.support.test.runner.AndroidJUnit4;
 
@@ -10,22 +8,11 @@ import org.junit.Test;
 import org.junit.runner.RunWith;
 
 import ajitsingh.com.expensemanager.R;
-import ajitsingh.com.expensemanager.rule.DatabaseResetRule;
 
-import static android.support.test.InstrumentationRegistry.getInstrumentation;
-import static android.support.test.espresso.Espresso.onData;
 import static android.support.test.espresso.Espresso.onView;
-import static android.support.test.espresso.Espresso.openActionBarOverflowOrOptionsMenu;
-import static android.support.test.espresso.Espresso.pressBack;
-import static android.support.test.espresso.action.ViewActions.click;
-import static android.support.test.espresso.action.ViewActions.typeText;
 import static android.support.test.espresso.assertion.ViewAssertions.matches;
-import static android.support.test.espresso.matcher.ViewMatchers.isDisplayed;
 import static android.support.test.espresso.matcher.ViewMatchers.withId;
 import static android.support.test.espresso.matcher.ViewMatchers.withText;
-import static org.hamcrest.CoreMatchers.instanceOf;
-import static org.hamcrest.CoreMatchers.is;
-import static org.hamcrest.core.AllOf.allOf;
 
 
 @RunWith(AndroidJUnit4.class)
@@ -33,46 +20,8 @@ public class MainActivityTest {
   @Rule
   public ActivityTestRule activityTestRule = new ActivityTestRule(MainActivity.class);
 
-  @Rule
-  public DatabaseResetRule databaseResetRule = new DatabaseResetRule();
-
   @Test
   public void shouldDisplayExpenseButton() throws Exception {
-    activityTestRule.launchActivity(new Intent());
     onView(withId(R.id.add_expense)).check(matches(withText("Add Expense")));
-  }
-
-  @Test
-  public void shouldAddExpenseWithNewExpenseType() throws Exception {
-    activityTestRule.launchActivity(new Intent());
-    openActionBarOverflowOrOptionsMenu(getInstrumentation().getTargetContext());
-    onView(withText(R.string.action_add_category)).perform(click());
-
-    final String expenseType = "Hello";
-    onView(withId(R.id.category)).perform(typeText(expenseType));
-    onView(withId(R.id.add_category)).perform(click());
-    pressBack();
-
-    onView(withId(R.id.amount)).perform(typeText("100"));
-    onView(withId(R.id.expense_type)).perform(click());
-    onData(allOf(is(instanceOf(String.class)), is(expenseType))).perform(click());
-
-    onView(withId(R.id.add_expense)).perform(click());
-
-    onView(withText(expenseType + " - 100")).check(matches(isDisplayed()));
-  }
-
-  @Test
-  public void shouldAddExpenseVerifyOnWeekExpenseDetails() throws Exception {
-    activityTestRule.launchActivity(new Intent());
-    onView(withId(R.id.amount)).perform(typeText("100"));
-    onView(withId(R.id.expense_type)).perform(click());
-    onData(allOf(is(instanceOf(String.class)), is("Health"))).perform(click());
-    onView(withId(R.id.add_expense)).perform(click());
-
-    DrawerActions.openDrawer(R.id.drawer);
-
-    onView(withText("This Week")).perform(click());
-    onView(withText("Total Expense - \u20B9100"));
   }
 }
